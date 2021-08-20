cfc2d9ac671acf0e4d50ac5a90b3966f71ff2690
sanyabeer <a.j.pivovarov@gmail.com>
2021-08-20 15:17:48 +0300

    add async await

--- reader.js
+++ reader.js
@@ -5,6 +5,19 @@ const {
 
 const chunkCount = 4;
 
+const combineChunks = async (promises) => {
+    const values = await Promise.all(promises);
+    fs.writeFile(
+        'chunkSorted.json',
+        JSON.stringify(
+            values.flat(1)
+                .sort((a, b) => a.name.localeCompare(b.name))
+        ),
+        {encoding: "utf8"},
+        (err) => { if (err) throw err; }
+    )
+}
+
 if (isMainThread) {
     console.time()
     const promises = [];
@@ -32,21 +45,11 @@ if (isMainThread) {
             )
         )
     }
-    Promise.all(promises).then(
-        (values) => {
-            fs.writeFile(
-                'chunkSorted.json',
-                JSON.stringify(
-                    values.flat(1)
-                        .sort((a, b) => a.name.localeCompare(b.name))
-                ),
-                {encoding: "utf8"},
-                (err) => {}
-            )
-            console.timeEnd()
-        }
-    )
+    combineChunks(promises)
+        .then(() => console.timeEnd())
+        .catch((e) => console.error(e));
 } else {
+    console.log('Child thread')
     fs.readFile(
         'chunk'+workerData+'.json',
         'utf8',
